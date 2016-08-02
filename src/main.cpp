2407adba712077904718257b9d7de8166130acc1
lhwanglj <lh_wanglijun@hotmail.com>
2016-08-02 14:50:20 +0800

    add sourc files

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <MediaIO/IMediaIO.h>
+#include <MediaIO/net/srs_librtmp.h>
+#include <audiomixer.h>
+
+
+#include <wavreader.h>
+#include <i420reader.h>
+#include <i420writer.h>
+#include <audiocodec.h>
+#include <videocodec.h>
+#include <videofilter.h>
+#include <picturemixer.hpp>
+
+
+
+using namespace AVMedia;
+using namespace AVMedia::MediaIO;
+using namespace MediaCloud::Adapter;
+
+int main(int argc, char** argv)
+{
+    printf("hello world.\r\n");
+   
+
+    return 1;
+}
